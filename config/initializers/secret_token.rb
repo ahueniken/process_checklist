83cbd270eca27b3245f9ac79d0deb3574c709444
Adam Hueniken <adam.hueniken@gmail.com>
2012-10-13 19:54:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChecklistApp::Application.config.secret_token = 'be3fcfed0bfb9ab685e1a6371e22915ab27cd217d9922b3ca5c9fa2f0774a1dd9ac08484c305df70c51acce80672efe5798e2a204910a896f733ebf9ef224a2e'
